b81a6ff3fdb3623e4fbe5bf18e8cca42797e6fa5
Rinat Kamaletdinov <sky2208@ya.ru>
2021-04-05 21:45:46 +0400

    fix

--- src/index.test.js
+++ src/index.test.js
@@ -1,16 +1,17 @@
-import GameSavingLoader from "./index";
-import json from "./json";
-import read from "./read";
+import GameSavingLoader from './index';
+import json from './json';
+import read from './read';
 
-test('promise',()=> {
-    let obj = new GameSavingLoader(); 
-        obj.load().then(item => { json(item).then( i=> {
-            expect(i).toEqual({"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}});
-        });
-    });    
+test('promise', () => {
+  GameSavingLoader.load().then((item) => {
+    json(item).then((i) => {
+      expect(i).toEqual({
+        id: 9,
+        created: 1546300800,
+        userInfo: {
+          id: 1, name: 'Hitman', level: 10, points: 2000,
+        },
+      });
+    });
+  });
 });
-
-
-
-
-
